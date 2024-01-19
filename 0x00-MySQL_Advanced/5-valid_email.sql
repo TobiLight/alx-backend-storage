0403eff9e43c05b91b892b515c15de0a3874573b
TobiLight <jgetitdonefast@gmail.com>
2024-01-19 02:11:20 +0100

    ✔ SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,16 +2,17 @@
 -- only when the email has been changed.
 DROP TRIGGER IF EXISTS validate_email;
 
-DELIMITER $$
+DELIMITER $$;
 
 CREATE TRIGGER validate_email BEFORE UPDATE ON users 
 FOR EACH ROW 
 BEGIN 
-	IF OLD.email ! = NEW.email THEN SET NEW.valid_email = 0;
+IF OLD.email <> NEW.email THEN SET NEW.valid_email = 0;
 	ELSE SET NEW.valid_email = NEW.valid_email;
 END
 	IF;
-END
-$$ 
+END;
+
+$$
 
 DELIMITER;
