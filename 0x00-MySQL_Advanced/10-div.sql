8b45158d2fb2929e61dae794912284112fb0ce5c
TobiLight <jgetitdonefast@gmail.com>
2024-01-19 02:16:31 +0100

    ✔  SQL script that creates a function SafeDiv

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,22 +2,18 @@
 -- by the second number or returns 0 if the second number is equal to 0.
 DROP FUNCTION IF EXISTS SafeDiv;
 
-DELIMITER $$
+DELIMITER $$;
 
 CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT 
 DETERMINISTIC 
 BEGIN 
 DECLARE
 	result FLOAT DEFAULT 0;
-	IF b ! = 0 THEN SET result = a
-
-
-b;
+IF b <> 0 THEN SET result = a b;
 
 END IF;
 
 RETURN result;
-
 END $$
 
 DELIMITER;
