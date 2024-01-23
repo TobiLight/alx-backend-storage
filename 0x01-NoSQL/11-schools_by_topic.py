6a0dbc5021c2c56f1410cd295a4e7a3562a5afc4
TobiLight <jgetitdonefast@gmail.com>
2024-01-24 00:03:03 +0100

    ✔ Returns the list of school having a specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,5 +8,9 @@ def schools_by_topic(mongo_collection, topic):
     """
     Returns the list of school having a specific topic
     """
-    data = mongo_collection.find({'topic': topic})
-    return list(data)
+    data = mongo_collection.find({'topics': {
+        '$elemMatch': {
+            '$eq': topic,
+        },
+    }})
+    return [data_ for data_ in data]
