d80163bbb33d47aa27fdf071fcac6bb417883719
TobiLight <jgetitdonefast@gmail.com>
2024-01-24 00:14:34 +0100

    ✔ Prints stats about Nginx request logs.

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -11,13 +11,11 @@ def print_nginx_request_logs(nginx_collection):
     """
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
-
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
     for method in methods:
         req_count = len(list(nginx_collection.find({'method': method})))
         print('\tmethod {}: {}'.format(method, req_count))
-
     status_checks_count = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
