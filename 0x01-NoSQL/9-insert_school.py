881e56aa801a9b22e0d0b02a40638ebc4df4c41d
TobiLight <jgetitdonefast@gmail.com>
2024-01-23 23:48:09 +0100

    ✔ Inserts a new document in a collection

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Insert school module
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection
+    """
+    data = mongo_collection.insert_one(kwargs)
+    return data._id
