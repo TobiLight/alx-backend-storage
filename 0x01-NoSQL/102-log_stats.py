5f7bc8a9907c29e8ab571d62b02555c541f07c49
TobiLight <jgetitdonefast@gmail.com>
2024-01-24 02:45:56 +0100

    ✔ 10 of the most present IPs in the collection nginx of the database logs

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -9,16 +9,14 @@ def print_nginx_request_logs(nginx_collection):
     """
     Prints stats about Nginx request logs.
     """
-    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('{} logs'.format(nginx_collection.estimated_document_count()))
     print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
+    for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+        req_count = nginx_collection.count_documents({"method": method})
         print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
+    status_checks_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
     print('{} status check'.format(status_checks_count))
 
 
