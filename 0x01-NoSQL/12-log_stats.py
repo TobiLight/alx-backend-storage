a3af7d6d17566361628fff38dda2496b2113768c
TobiLight <jgetitdonefast@gmail.com>
2024-01-24 02:43:57 +0100

    ✔ Prints stats about Nginx request logs.

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,28 +9,17 @@ def print_nginx_request_logs(nginx_collection):
     """
     Prints stats about Nginx request logs.
     """
-    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('{} logs'.format(nginx_collection.estimated_document_count()))
     print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
+    for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+        req_count = nginx_collection.count_documents({"method": method})
         print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
+    status_checks_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
     print('{} status check'.format(status_checks_count))
 
 
-def run():
-    """
-    Provides some stats about Nginx logs stored in MongoDB.
-    """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client['logs']
-    collection = db['nginx']
-    print_nginx_request_logs(collection)
-
-
 if __name__ == '__main__':
-    run()
+    client = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    print_nginx_request_logs(client)
