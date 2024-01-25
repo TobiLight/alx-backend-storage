8963be8050fefabd68f66c957d5b8800bb7ab8f8
TobiLight <jgetitdonefast@gmail.com>
2024-01-25 01:57:11 +0100

    ✔ Created a decorator method call_history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,6 +25,37 @@ def count_calls(method: Callable) -> Callable:
 
     return wrapper
 
+
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator that stores the history of inputs and outputs for a function in Redis.
+
+    Args:
+        method (Callable): The method to be decorated.
+
+    Returns:
+        Callable: The decorated method.
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Creating keys for input and output lists
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        # Storing input arguments
+        self._redis.rpush(input_key, str(args))
+
+        # Executing the wrapped function to get the output
+        result = method(self, *args, **kwargs)
+
+        # Storing the output in the Redis list
+        self._redis.rpush(output_key, str(result))
+
+        # Returning the output
+        return result
+
+    return wrapper
+
 class Cache:
     """Represents an object for storing data in a Redis data storage."""
 
@@ -33,6 +64,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores a value in a Redis data storage and returns the key."""
