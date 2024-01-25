d0fe2c1e080d6572959b4a71a2aec4b82f2acacc
TobiLight <jgetitdonefast@gmail.com>
2024-01-25 02:34:35 +0100

    ✔ Web request caching and tracking tool

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,33 +11,27 @@ import requests
 redis_conn = redis.Redis()
 
 
-def data_cacher(func: Callable) -> Callable:
-    """Caches the output of a fetched data"""
-    @wraps(func)
-    def wrapper(url: str) -> str:
-        """Wrapper function for caching the output"""
-        # Increment access count
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
         redis_conn.incr(f'count:{url}')
-
-        # Check if the result is already cached
-        cached_result = redis_conn.get(f'result:{url}')
-        if cached_result:
-            return cached_result.decode('utf-8')
-
-        # Call the original function and cache the result
-        result = func(url)
+        result = redis_conn.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
         redis_conn.set(f'count:{url}', 0)
         redis_conn.setex(f'result:{url}', 10, result)
-
         return result
-    return wrapper
+    return invoker
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    """
-    Returns the content of a URL after caching the request's response,
+    '''Returns the content of a URL after caching the request's response,
     and tracking the request.
-    """
-    response = requests.get(url)
-    return response.text
+    '''
+    return requests.get(url).text
